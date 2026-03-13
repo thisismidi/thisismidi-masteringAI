a7aff28e9ff61475c42698e472cca77da2cc4811
thisismidi <itsfreiar@gmail.com>
2026-03-13 15:22:52 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   eslint: {
-    // Vercel 빌드 시 ESLint 경고 무시
+    // Vercel 배포 시 깐깐한 문법 에러 무시
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Vercel 빌드 시 타입스크립트 에러 무시
+    // Vercel 배포 시 깐깐한 타입 에러 무시
     ignoreBuildErrors: true,
   },
 };
