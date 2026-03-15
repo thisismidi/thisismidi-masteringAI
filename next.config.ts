bb224365920a9a737b9f2df3f183f26f249c1452
thisismidi <itsfreiar@gmail.com>
2026-03-16 03:20:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
     // 클라이언트(브라우저) 사이드에서 렌더링할 때 fs 모듈 에러 무시
     if (!isServer) {
       config.resolve.fallback = {
@@ -10,7 +11,7 @@ const nextConfig = {
     }
     return config;
   },
-  // 🚨 Next.js 16 터보팩 빌드 에러 우회용 설정 추가
+  // 🚨 Next.js 16 터보팩 빌드 에러 우회용 설정
   turbopack: {},
 };
 
