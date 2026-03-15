b268666e503e5cbf46526bcb6b322e8d3fddb321
thisismidi <itsfreiar@gmail.com>
2026-03-16 03:14:33 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // 클라이언트(브라우저) 사이드에서 렌더링할 때 fs 모듈 에러 무시
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
+
 export default nextConfig;
